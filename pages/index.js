fd5b003ee5ec771bf036bea7324b01f0cc6bf388
basilmaben <basilmaben.bm@gmail.com>
2021-10-16 19:43:21 +0530

    Header and NavBar completed

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,19 @@
 import Head from 'next/head'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="">
       <Head>
         <title>Instagram</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Insta build</h1>
+      {/* Header */}
+        <Header/>
+      {/* Feed */}
+
+      {/* Modal */}
     </div>
   )
 }
